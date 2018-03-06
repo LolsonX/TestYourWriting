db16a82fbedf5f548ca5b56c325030048e401bb0
Karol Ostrowski <xlolsonx@gmail.com>
2018-03-06 19:12:52 +0100

    Added some style

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QTimer, QTime
 import sys
 import random
-import time
 
 
 class WordGenerator:
@@ -27,13 +27,14 @@ class WordGenerator:
 class WritingTest(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.clock = QTimer()
+        self.time = QTime(0, 0, 0)
         self.userInput = QLineEdit()
         self.generatedWord = QLineEdit()
         self.generateButton = QPushButton()
         self.grid = QGridLayout()
         self.draw_interface()
         self.words = WordGenerator()
-        self.clock = 0
 
     def draw_interface(self):
         self.grid.addWidget(self.generatedWord, 1, 0)
@@ -57,6 +58,7 @@ class WritingTest(QWidget):
         self.generateButton.clicked.connect(self.make_action)
         self.generateButton.setShortcut("Return")
         self.userInput.returnPressed.connect(self.make_action)
+        self.clock.timeout.connect(self.timeout)
 
     def make_action(self):
         source = self.sender()
@@ -65,13 +67,24 @@ class WritingTest(QWidget):
             self.generatedWord.setText(word)
             self.userInput.grabKeyboard()
             self.generateButton.setDisabled(True)
-            self.clock = time.time()
+            self.clock.start(1000)
         if source.text() == self.generatedWord.text():
             self.generatedWord.clear()
             self.userInput.clear()
             self.generateButton.setDisabled(False)
-            self.clock = time.time() - self.clock
+            self.clock.stop()
+            self.time = QTime(0, 0, 0)
             tmp = QMessageBox.about(self, "Win", "You won!")
+
+    def timeout(self):
+        self.time = self.time.addSecs(1)
+        if self.time.second() == 10:
+            tmp = QMessageBox.critical(self, "Lose", "You've lost!")
+            self.generatedWord.clear()
+            self.userInput.clear()
+            self.generateButton.setDisabled(False)
+            self.clock.stop()
+            self.time = QTime(0, 0, 0)
 
 
 if __name__ == "__main__":
